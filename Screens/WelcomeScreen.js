870b3aef4e6aae5d3ab22e2762da26aa2a8a0ccd
Valentino Junior <ojvalentine14@gmail.com>
2024-05-13 18:01:35 +0300

    Added welcome login and signup in the welcome screen

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function WelcomeScreen() {
+const WelcomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <Text>Welcome to My App!</Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="Signup"
+        onPress={() => navigation.navigate('Signup')}
+      />
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default WelcomeScreen;
