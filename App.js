f6d532c89409e6498d210abc87c8b87480f7c2a3
Valentino Junior <ojvalentine14@gmailcom>
2024-05-13 16:50:22 +0300

    Imported screens to appjs

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './Screens/HomeScreen';
+import LoginScreen from './Screens/LoginScreen';
+import SignupScreen from './Screens/SignupScreen';
+import WelcomeScreen from './Screens/WelcomeScreen';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
