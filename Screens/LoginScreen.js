a966086b3f9c88cd5a57146152f2b6dcec2e8377
Valentino Junior <ojvalentine14@gmail.com>
2024-05-13 18:04:55 +0300

    Added login inputs

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// LoginScreen.js
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
-export default function LoginScreen() {
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Perform login logic here
+    // For example, you can check the credentials against a database
+    // If login is successful, navigate to HomeScreen
+    navigation.navigate('Home');
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
+export default LoginScreen;
