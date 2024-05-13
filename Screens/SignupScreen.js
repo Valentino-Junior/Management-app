622c2c9186efe5dc712d55e0135e1678a568f83d
Valentino Junior <ojvalentine14@gmailcom>
2024-05-13 18:14:49 +0300

    Added signup inputs

--- Screens/SignupScreen.js
+++ Screens/SignupScreen.js
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// SignupScreen.js
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
-export default function SignupScreen() {
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = () => {
+    // Perform signup logic here
+    // For example, you can create a new user account
+    // If signup is successful, navigate to HomeScreen
+    navigation.navigate('Home');
+  };
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
+    <View style={styles.container}>
+      <Text>Sign Up</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Sign Up" onPress={handleSignup} />
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
+export default SignupScreen;
