9148ff6f33d884338603c5746cd7feceaa87a39b
Valentino Junior <ojvalentine14@gmail.com>
2024-05-15 07:05:45 +0300

    Added drawer to the homescreen

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function ProfileScreen() {
+const ProfileScreen = () => {
   return (
     <View>
       <Text>ProfileScreen</Text>
     </View>
   )
 }
+
+export default ProfileScreen
 
 const styles = StyleSheet.create({})
