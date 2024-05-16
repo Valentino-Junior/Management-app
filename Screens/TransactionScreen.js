96a37b86a058dc37b32594274dd47b74708d30f0
Valentino Junior <ojvalentine14@gmail.com>
2024-05-16 03:58:51 +0300

    Defined the transaction screen component

--- Screens/TransactionScreen.js
+++ Screens/TransactionScreen.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Transactions = () => {
+const TransactionScreen = () => {
   return (
     <View>
       <Text>Transactions</Text>
